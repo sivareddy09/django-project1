95df3c64ae5cb67784531cf39f6f86d79f0c10ab
siva <gspr991@gmail.com>
2024-06-20 11:21:25 +0530

    new changes

--- employees/views.py
+++ employees/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from .models import Employee
 from .forms import EmployeeForm
 
